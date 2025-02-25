3d80d09452de38912733d546d1f8fc9c64398dd4
Orudnev <olrudnev@gmail.com>
2025-02-25 18:01:59 +0600

    Шаблон проекта

--- src/app.tsx
+++ src/app.tsx
@@ -1,12 +1,4 @@
 import React, { useEffect, useState } from 'react';
-import {
-  Routes,
-  Route,
-  NavigateFunction,
-  useNavigate,
-  Navigate,
-  Link
-} from "react-router-dom";
 
 
 function App() {
@@ -15,10 +7,7 @@ function App() {
   }, []);
   return (
     <div>
-      <Routes>
-        <Route path={"/"} element={<div>Root</div>} />
-        <Route path={"/S1"} element={<div>S1</div>} />
-      </Routes> 
+        blablabla
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './app';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import {store} from './Reducers/index';
 
@@ -12,9 +12,7 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <Provider store={store}>
-    <BrowserRouter>
       <App />
-    </BrowserRouter>    
   </Provider>
 );
 
