71570be0a4ed7946d418fc5071c892fc812d057a
Orudnev <olrudnev@gmail.com>
2025-11-18 15:23:32 +0600

    версия

--- src/common-types.tsx
+++ src/common-types.tsx
@@ -2,7 +2,7 @@ import { all } from "axios";
 import { GeneratePseudoUniqueId } from "./web-api-wrapper";
 import { RiceBowl } from "@mui/icons-material";
 
-export const appVersion = "1.0.5";
+export const appVersion = "1.0.6";
 
 export enum TableNameEnum {
     None = "",
